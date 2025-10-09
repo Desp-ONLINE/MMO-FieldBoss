9ad1219751ee5ddd4e43bf216f766deea5dbec03
nao505 <nao505@naver.com>
2025-10-09 20:53:51 +0900

    Add `@Getter` annotation to FieldBoss events
    Apply `@Getter` annotation to streamline access to fields and improve code consistency in FieldBoss-related events.

--- src/main/java/com/binggre/mmofieldboss/api/FieldBossEvent.java
+++ src/main/java/com/binggre/mmofieldboss/api/FieldBossEvent.java
@@ -7,6 +7,7 @@ import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
 import org.jetbrains.annotations.NotNull;
 
+@Getter
 public abstract class FieldBossEvent extends Event {
 
     private static final HandlerList handlers = new HandlerList();
